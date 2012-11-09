c8d057c49b493de8c7bb8ddf6f72aa992da72085
Carlo Minucci <gecco@napodano.com>
2012-11-09 20:42:37 +0100

    aggiunto informazioni al file version.php

--- version.php
+++ version.php
@@ -3,5 +3,6 @@ $filename = "VERSION";
 $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
+echo "<a href=\"http://code.google.com/p/gestiolus/\">Gestiolus </a>";
 echo "v. " . $contents;
 ?>
