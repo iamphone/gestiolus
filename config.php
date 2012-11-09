f5a79342ff90435db407aa3d2320ef58561a9156
Carlo Minucci <gecco@napodano.com>
2012-11-09 20:19:19 +0100

    sistemato, per essere più leggibile da mani inesperte, il file di configurazione

--- config.php
+++ config.php
@@ -1,5 +1,13 @@
 <?php
-$title = "Gestione interventi";
-$link = mysql_connect('_HOST_', '_USER_', '_PASSWORD_');
-mysql_select_db("gestiolus");
+// persolizza i tuoi parametri
+$title = "_TITLE_";
+$hostname = "_HOST_";
+$username = "_USER_";
+$password = "_PASSWORD_";
+$database = "_DBNAME_";
+
+// non toccare niente qua sotto
+
+$link = mysql_connect($hostname, $username, $password);
+mysql_select_db($database);
 ?>
