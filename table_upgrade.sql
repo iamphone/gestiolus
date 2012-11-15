dbc5073f27b45150af46dc60cfa14d5e4f498ebc
carlo <carlo@tec2.volta.fano>
2012-11-15 12:50:40 +0100

    aggiunto il controllo di versione giusto

--- table_upgrade.sql
+++ table_upgrade.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS `notepad` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `note` text NOT NULL,
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
